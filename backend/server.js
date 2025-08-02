6f22e8e0d874afe13763830ea9221c11fd751a00
srikanthnairm63 <srikanthm63@gmail.com>
2025-08-02 13:53:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,17 @@ const bodyParser = require('body-parser');
 const userRoutes = require('./routes/users');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
+
+if (!PORT) {
+  throw new Error('PORT environment variable is not defined');
+}
 
 app.use(cors());
 app.use(bodyParser.json());
-
 app.use('/api/users', userRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
+
